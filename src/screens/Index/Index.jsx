b23bb24ebabdb28dcbb66e287efc1a70db02dd5e
Udeesha rukshan <76590372+UdeeshaRukshan@users.noreply.github.com>
2023-08-26 16:28:36 +0530

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="text-wrapper">AboAbout Us</div>
+          <div className="rectangle" />
+          <div className="text-wrapper-2">Home</div>
+          <img
+            className="line"
+            alt="Line"
+            src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/line-1.svg"
+          />
+          <img
+            className="auction-pal"
+            alt="Auction pal"
+            src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/auctionpal.png"
+          />
+          <div className="text-wrapper-3">Categories</div>
+          <img
+            className="img"
+            alt="Line"
+            src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/line-2.svg"
+          />
+          <div className="rectangle-2" />
+          <div className="text-wrapper-4">Sign Up</div>
+          <div className="rectangle-3" />
+          <img
+            className="auction-pal-2"
+            alt="Auction pal"
+            src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/auctionpal-1.png"
+          />
+          <div className="text-wrapper-5">About us</div>
+        </div>
+        <div className="overlap">
+          <div className="text-wrapper-6">Photography Guide</div>
+          <div className="text-wrapper-7">Submit your Collectables</div>
+          <div className="text-wrapper-8">How it works</div>
+          <div className="text-wrapper-9">FAQs</div>
+          <div className="div-wrapper">
+            <div className="text-wrapper-10">AuctionPal</div>
+          </div>
+          <div className="text-wrapper-11">Buying a car</div>
+          <div className="text-wrapper-12">Selling a car</div>
+          <div className="text-wrapper-13">Finalizing the sale</div>
+          <div className="text-wrapper-14">Sellers</div>
+          <div className="text-wrapper-15">Submit your vehicle</div>
+          <div className="text-wrapper-16">Submit your property</div>
+          <div className="text-wrapper-17">Submit your ART</div>
+          <p className="copyright">
+            <span className="span">
+              Copyright 2023 AuctionPal LLC
+              <br />
+            </span>
+            <span className="text-wrapper-18">Terms of Use</span>
+            <span className="span">&nbsp;</span>
+            <span className="text-wrapper-18">Privacy Policy</span>
+          </p>
+          <div className="overlap-2">
+            <div className="ellipse" />
+            <div className="text-wrapper-19">c</div>
+          </div>
+          <div className="overlap-3">
+            <img
+              className="image"
+              alt="Image"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/image-9@2x.png"
+            />
+            <img
+              className="image-2"
+              alt="Image"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/image-10@2x.png"
+            />
+            <img
+              className="image-3"
+              alt="Image"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/image-11@2x.png"
+            />
+            <img
+              className="image-4"
+              alt="Image"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9b85f508fe1ef25bcbfb6/img/image-12@2x.png"
+            />
+          </div>
+        </div>
+        <div className="overlap-4">
+          <div className="rectangle-4" />
+          <div className="component">
+            <div className="LOGIN">Login</div>
+            <div className="rectangle-5" />
+            <div className="rectangle-6" />
+            <div className="frame-wrapper">
+              <div className="frame">
+                <div className="text-wrapper-20">Login</div>
+              </div>
+            </div>
+            <div className="text-wrapper-21">Username</div>
+            <div className="text-wrapper-22">Password</div>
+            <div className="text-wrapper-23">Forgot Password?</div>
+            <img
+              className="element-ford-f"
+              alt="Element ford f"
+              src="https://anima-uploads.s3.amazonaws.com/projects/64e70879208aa4924891216d/releases/64e9d98ab6adb5ab939b9704/img/2023-ford-f-150-raptor-r-109-1657904944-1.png"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
